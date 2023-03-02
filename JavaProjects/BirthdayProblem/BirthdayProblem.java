16043d81c3da1453075cc49152e80d4133675c13
gloryc34 <114937071+gloryc34@users.noreply.github.com>
2023-03-02 15:23:15 -0500

    Add files via upload

---
+++ JavaProjects/BirthdayProblem/BirthdayProblem.java
@@ -0,0 +1,14 @@
+/**
+ * @author Colin Glory
+ * @version Version: 2022-12 (4.26.0)
+ */
+public class BirthdayProblem {
+	
+	public static void main(String[] args) {
+		
+        BirthdayProblemTester tester = new BirthdayProblemTester();
+        tester.run();
+        
+    }
+}
+
